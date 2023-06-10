a50124a2912ae042f4aa3c8420ede62cdb7758c6
ericcorona1 <ericcorona3@gmail.com>
2023-06-10 10:34:48 -0700

    added del button

--- src/App.jsx
+++ src/App.jsx
@@ -12,6 +12,9 @@ function App() {
         <div className="previous-operand"></div>
         <div className="current-operand"></div>
       </div>
+      <button className="span-two">AC</button>
+      <button>DEL</button>
+      <button></button>
     </div>
   );
 }
