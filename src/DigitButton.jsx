0e293b65ab9a0e833d09184f0e213f8ffd65ccc2
ericcorona1 <ericcorona3@gmail.com>
2023-06-12 12:39:24 -0700

    working on fixing the operand buttons
    removed misplaced quotes

--- src/DigitButton.jsx
+++ src/DigitButton.jsx
@@ -2,7 +2,7 @@ import { ACTIONS } from "./App.jsx";
 
 export default function DigitButton({ dispatch, digit }) {
   return (
-    <button onClick="{() => dispatch({type: ACTIONS.ADD_DIGIT, payload: { digit } })}">
+    <button onClick={() => dispatch({type: ACTIONS.ADD_DIGIT, payload: { digit } })}>
       {digit}
     </button>
   );
